a7d41e6844b76c6c701eb2a0c40bde60221e5ac5
kamalpandey27 <kamal0000027@gmail.com>
2025-04-11 15:45:04 +0530

    add page

--- Diagnostic Lab Website/src/components/Home.jsx
+++ Diagnostic Lab Website/src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
       <main>
         {/* Hero Section */}
         <section
-          className={` overflow-x-hidden h-[calc(100vh-60px)] w-screen  bg-cover bg-center relative  `}
+          className={` overflow-x-hidden h-screen w-screen  bg-cover bg-center relative  `}
           style={{ backgroundImage: `url(${heroImage})` }}
         >
           <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
@@ -38,15 +38,15 @@ function Home() {
               </h2>
             </div>
             <div className="flex items-center">
-              <button className="cursor-pointer w-[160px] ">
+              <button className="cursor-pointer sm:w-[160px] w-[130px] sm:text-[16px] text-[14px] ">
                 <NavLink
                   to="/AppointmentForm"
-                  className="w-full bg-blue-500  text-white px-4 py-3 rounded hover:bg-white hover:text-blue-800   hover:border-1"
+                  className="w-full bg-blue-500  text-white sm:px-4 sm:py-3 px-2 py-2 rounded hover:bg-white hover:text-blue-800   hover:border-1"
                 >
                   Get Appointment
                 </NavLink>
               </button>
-              <button className="cursor-pointer w-[140px] ">
+              <button className="cursor-pointer w-[140px] sm:text-[16px] text-[14px]  ">
                 <NavLink
                   to="/AppointmentForm"
                   className={({ isActive }) =>
@@ -54,7 +54,7 @@ function Home() {
                       isActive
                         ? "bg-blue-800 text-white font-semibold"
                         : "bg-gray-700 text-white"
-                    } w-full  px-4 py-3 rounded hover:bg-blue-600 hover:text-white hover:font-semibold   `
+                    } w-full sm:px-4 sm:py-3 px-2 py-2 rounded hover:bg-blue-600 hover:text-white hover:font-semibold   `
                   }
                 >
                   Learn More
@@ -66,8 +66,46 @@ function Home() {
         {/* Hero Section Ends Here */}
 
         {/* Lab Time Section  */}
-        <section>
-          <div></div>
+        <section className="w-screen h-screen p-5 bg-white sm:relative md:h-[45vh] sm:h-[55vh]">
+          <div className=" flex justify-center items-center flex-wrap lg:gap-30 md:gap-15 gap-8 sm:absolute sm:-top-13 h-full ">
+            <div className="bg-blue-500 rounded text-white md:p-7 sm:w-[40%] w-full p-5  h-[280px] md:h-[280px] sm:h-[290px] flex flex-col gap-7">
+              <div className="font-bold text-xl">Opening Time</div>
+
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                  <div>Monday - Friday</div>
+                  <div>8AM - 9PM</div>
+                </div>
+
+                <div className="flex justify-between">
+                  <div>Saturday</div>
+                  <div>9AM - 6PM</div>
+                </div>
+                <div className="flex justify-between">
+                  <div>Sunday</div>
+                  <div>Holiday</div>
+                </div>
+              </div>
+              <button className="bg-white w-[120px] rounded m-auto text-blue-500 font-semibold px-3 py-2">
+                Learn More
+              </button>
+            </div>
+
+            <div className="bg-blue-500 rounded text-white md:p-7 sm:w-[40%] w-full p-5 h-[265px] md:h-[280px] sm:h-[290px] flex flex-col justify-between  lg:gap-7 gap-3">
+              <div className="font-bold text-xl">Emergency Cases</div>
+              <div className="md:text-[15px]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
+                fugit repellat distinctio. Possimus quos quasi voluptas
+                doloribus,Lorem ipsum dolor sit amet consectetur adipisicing
+                elit.
+              </div>
+              <div className="flex justify-center items-center">
+                <button className="bg-white w-[120px] rounded m-auto text-blue-500 font-semibold px-3 py-2">
+                  Learn More
+                </button>
+              </div>
+            </div>
+          </div>
         </section>
         {/* Lab Time Section Ends */}
       </main>
