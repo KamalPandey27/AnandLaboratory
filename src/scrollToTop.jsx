d87e7fc64bde484801c3016d3456c246a0c205dd
kamalpandey27 <kamal0000027@gmail.com>
2025-04-18 17:59:49 +0530

    smooth scroll to top

--- src/scrollToTop.jsx
+++ src/scrollToTop.jsx
@@ -3,11 +3,16 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
+    // Smooth scroll to top on route change
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth", // <-- this is the magic line
+    });
+  }, [pathname]);
 
   return null;
 };
