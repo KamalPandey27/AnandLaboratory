116fb6dc4755b1f2dfd436c378bf76d55fbcd848
kamalpandey27 <kamal0000027@gmail.com>
2025-04-21 01:24:37 +0530

    c

--- DiagnosticLabWebsite/vite.config.js
+++ DiagnosticLabWebsite/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
     proxy: {
-      "/formdata": "http://localhost:3000/formdata",
+      "/formdata": "http://localhost:3000",
     },
   },
   plugins: [react(), tailwindcss()],
