22acfcd813a589d25e517ec3eebc3b9226eaa23c
kamalpandey27 <kamal0000027@gmail.com>
2026-02-12 13:54:50 +0530

    feat: enhance CORS configuration to support dynamic origin and credentials

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,12 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
